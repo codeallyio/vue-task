3cf8e545e29a062e945dc82189d0c2944619c6cf
Adam Jędrzejec <adamjedrzejec@gmail.com>
2020-10-26 17:42:42 +0100

    [add] stroveio init

---
+++ init.sh
@@ -0,0 +1,5 @@
+echo "\e[32m" #green
+echo "------------------------------------------"
+echo "\nSetup complete!\n\nRun the project using \`yarn serve\`\n"
+echo "------------------------------------------"
+echo "\e[0m" # green reset
